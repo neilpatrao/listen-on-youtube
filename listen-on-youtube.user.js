23a18a8257691029b82f93740fd17d164e9746d0
Neil Patrao <neilpatrao518@gmail.com>
2026-02-15 20:50:54 +0400

    Fixed button not appearing and updated button to match new UI

--- listen-on-youtube.user.js
+++ listen-on-youtube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Listen On YouTube
 // @namespace    http://tampermonkey.net/
-// @version      2025-03-26
+// @version      2026-02-15
 // @description  Adds "Listen on YouTube" button to YouTube Music.
 // @author       Neil Patrao
 // @match        *://music.youtube.com/*
@@ -60,18 +60,22 @@
     GM_addStyle(STYLES);
 
     const BUTTON = $(`
-        <ytlmusic-menu-navigation-item-renderer class="style-scope ytmusic-menu-popup-renderer iron-selected" role="menuitem" tabindex="0" aria-disabled="false" aria-selected="true">
-            <!--css-build:shady--><!--css-build:shady-->
+        <ytlmusic-menu-navigation-item-renderer class="style-scope ytmusic-menu-popup-renderer" role="menuitem" tabindex="-1" aria-disabled="false" aria-selected="false">
+            <!--css-build:shady-->
+            <!--css-build:shady-->
             <a id="navigation-endpoint" class="yt-simple-endpoint style-scope ytmusic-menu-navigation-item-renderer" tabindex="-1" target="_blank">
-                <ytl-icon class="icon style-scope ytmusic-menu-navigation-item-renderer">
-                    <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon">
-                        <g class="style-scope yt-icon">
-                            <path d="M 11.9895 18.9103 c 0 0 6.2653 0 7.8194 -0.4138 c 0.875 -0.2345 1.5331 -0.9103 1.7641 -1.7448 c 0.427 -1.5311 0.427 -4.7517 0.427 -4.7517 c 0 0 0 -3.2 -0.427 -4.7173 c -0.231 -0.8552 -0.8891 -1.5172 -1.7641 -1.7448 c -1.5541 -0.4207 -7.8194 -0.4207 -7.8194 -0.4207 c 0 0 -6.2513 0 -7.7984 0.4207 c -0.861 0.2276 -1.5331 0.8896 -1.7781 1.7448 c -0.413 1.5173 -0.413 4.7173 -0.413 4.7173 c 0 0 0 3.2206 0.413 4.7517 c 0.245 0.8345 0.9171 1.5103 1.7781 1.7448 C 5.7382 18.9103 11.9895 18.9103 11.9895 18.9103 z M 10 9 l 5 3 l -5 3 V 9 z" class="style-scope yt-icon"></path>
-                        </g>
-                    </svg>
-                    <!--css-build:shady--><!--css-build:shady-->
+                <ytl-icon class="icon style-scope ytmusic-menu-navigation-item-renderer" aria-hidden="" style="width: 18px; height: 18px;">
+                    <!--css-build:shady-->
+                    <!--css-build:shady-->
+                    <span class="yt-icon-shape style-scope yt-icon ytSpecIconShapeHost">
+                        <div style="width: 100%; height: 100%; display: block; fill: currentcolor;">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 18 18" width="18" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+                                <path d="M 8.9913 14.701 c 0 0 5.1689 0 6.451 -0.3414 c 0.7219 -0.1935 1.2648 -0.751 1.4554 -1.4395 C 17.25 11.657 17.25 9 17.25 9 c 0 0 0 -2.64 -0.3523 -3.8918 c -0.1906 -0.7055 -0.7335 -1.2517 -1.4554 -1.4395 c -1.2821 -0.3471 -6.451 -0.3471 -6.451 -0.3471 c 0 0 -5.1573 0 -6.4337 0.3471 c -0.7103 0.1878 -1.2648 0.7339 -1.4669 1.4395 c -0.3407 1.2518 -0.3407 3.8918 -0.3407 3.8918 c 0 0 0 2.657 0.3407 3.9202 c 0.2021 0.6885 0.7566 1.246 1.4669 1.4395 C 3.834 14.701 8.9913 14.701 8.9913 14.701 Z M 7.35 6.525 l 4.125 2.475 l -4.125 2.475 V 6.525 Z"></path>
+                            </svg>
+                        </div>
+                    </span>
                 </ytl-icon>
-                <ytl-formatted-string class="text style-scope ytmusic-menu-navigation-item-renderer">Listen on YouTube</ytl-formatted-string>
+                <ytl-formatted-string class="text style-scope ytmusic-menu-navigation-item-renderer">Listen on YouTube</yt-formatted-string>
             </a>
         </ytlmusic-menu-navigation-item-renderer>
     `);
@@ -108,17 +112,17 @@
     function getButton(listBox, buttonName) {
         const buttons = listBox.querySelectorAll("ytmusic-menu-navigation-item-renderer");
 
-        return Array.from(buttons).find(button => 
+        return Array.from(buttons).find(button =>
             button.querySelector("yt-formatted-string")?.innerHTML.trim() === buttonName
         );
     }
 
     function addButton(listBox) {
 
-        if (!document.querySelector("ytlmusic-menu-navigation-item-renderer") && getButton(listBox, "Start radio")) {
+        if (!document.querySelector("ytlmusic-menu-navigation-item-renderer") && getButton(listBox, "Start mix")) {
 
             const shareButton = getButton(listBox, "Share");
-            const radioButton = getButton(listBox, "Start radio");
+            const radioButton = getButton(listBox, "Start mix");
 
             BUTTON.find("a").attr("href", formatUrl(radioButton.querySelector("a").getAttribute("href")));
 
